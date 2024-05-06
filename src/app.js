a973c4aee65f9e9b4a25311f21a43d61638c44dd
Styoks <113370103+Styoks@users.noreply.github.com>
2024-05-06 19:07:50 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let excuse = document.getElementById("excuse");
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excuse.innerHTML = `${who[Math.floor(Math.random() * who.length)]} ${
+    action[Math.floor(Math.random() * action.length)]
+  } ${what[Math.floor(Math.random() * what.length)]} ${
+    when[Math.floor(Math.random() * when.length)]
+  }`;
 };
